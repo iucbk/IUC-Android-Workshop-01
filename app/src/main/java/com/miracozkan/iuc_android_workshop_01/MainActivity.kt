46a843bc5b1d6b859c51d3efcc0616589a02093f
Mirac Ozkan <mirac.ozkan123@gmail.com>
2019-11-12 09:02:35 +0300

    fist commit with <3

---
+++ app/src/main/java/com/miracozkan/iuc_android_workshop_01/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.miracozkan.iuc_android_workshop_01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
